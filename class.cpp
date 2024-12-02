d8d80d384a678dbaaf882fdeed9f984b2bb4b7f1
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2024-12-02 09:02:17 +0700

    edit file class.cpp

---
+++ class.cpp
@@ -0,0 +1,25 @@
+template <class T>
+class Node {
+private:
+  T data;
+  Node* next;
+};
+
+template <class T>
+class ILinkedList {
+private:
+  Node<T>* head;
+  Node<T>* tail;
+  int size
+
+public: 
+  public ILinkedList() = 0;
+  public ~ILinkedList() = 0;
+
+  public void add(T data) = 0;
+  public void remove(T data) = 0;
+  public void print() = 0;
+  public void clear() = 0;
+  public void size() = 0;
+  public void get(int idx) = 0;
+}
